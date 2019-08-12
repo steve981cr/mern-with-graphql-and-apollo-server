63a350f6f2f46a2b5f363e85079678c44ccc8de3
Steve Carey <steve981cr@gmail.com>
2019-08-12 12:32:33 -0700

    add express

--- server-express.js
+++ server-express.js
@@ -1,4 +1,10 @@
-const { ApolloServer } = require('apollo-server');
+/* 
+To use Apollo-server with an existing Express app install the following packages:
+  npm install express apollo-server-express
+*/
+
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
@@ -6,6 +12,8 @@ const resolvers = require('./graphql/resolvers');
 
 const PORT = 4000; 
 const MONGODB_URI = "mongodb://localhost:27017/my_local_db";   
+
+const app = express();
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false }); 
 mongoose.connection.once('open', function() { 
@@ -20,6 +28,9 @@ const server = new ApolloServer({
   resolvers 
 });
 
-server.listen(PORT).then(({ url }) => {
-  console.log(`Server listening at ${url}.`);
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen(PORT, function() { 
+  console.log(`Server listening on port ${PORT}.`);
 });
+ 
